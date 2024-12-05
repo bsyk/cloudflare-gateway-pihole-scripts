407e45d2a853b3a93eba246aabd456f4bcd924f0
Ben Sykes <ben@bensykes.com>
2024-12-05 09:17:33 -0800

    Add automation script that can run in a temp docker container on a cron schedule

---
+++ refresh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Fetch the latest changes from the remote repository
+git fetch origin
+git checkout auto
+git pull origin auto
+
+# Read the desired Node.js version from .node-version
+NODE_VERSION=$(cat .node-version)
+
+# Use the required Node.js version
+docker run --rm -v "$(pwd):/app" -w /app node:$NODE_VERSION-alpine npm run docker-refresh:blocklist
+
+# Clean any old docker images older than ~1 year
+# Unfortunately the prune script does not support a filter to restict to only node images
+# Scripting that can be done but is unneccesary here.
+docker image prune --all --force --filter "until=9000h"
